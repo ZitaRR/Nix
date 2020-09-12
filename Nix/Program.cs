6a2e698fe83d3d5a37a8746d5260f07e50518283
ZitaRR <Haeggqvist1998@hotmail.com>
2020-09-12 02:37:06 +0200

    Initial commit

---
+++ Nix/Program.cs
@@ -0,0 +1,17 @@
+﻿using Nix.Presenters;
+using System;
+using System.Drawing;
+using System.Reflection;
+
+namespace Nix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.Title = Assembly.GetExecutingAssembly().GetName().Name;
+            new HomePresenter();
+        }
+    }
+}
