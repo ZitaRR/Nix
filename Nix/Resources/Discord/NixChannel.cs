f6463998a31ea626f7159e315e33572203e485b0
ZitaRR <Haeggqvist1998@hotmail.com>
2020-09-17 06:40:15 +0200

    Added NixChannel

---
+++ Nix/Resources/Discord/NixChannel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nix.Resources
+{
+    public class NixChannel : IStorable
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public ulong ChannelID { get; set; }
+        public ulong GuildID { get; set; }
+        public DateTime StoredAt { get => DateTime.Now; }
+
+        public override string ToString()
+            => $"Name: {Name}\n" +
+            $"ID: {ID}\n" +
+            $"Channel ID: {ChannelID}\n" +
+            $"Guild ID: {GuildID}\n" +
+            $"Stored At: {StoredAt}";
+    }
+}
