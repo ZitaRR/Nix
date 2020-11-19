6da9b207a7e935a128d38f65661d8a5c491e25af
ZitaRR <Haeggqvist1998@hotmail.com>
2020-11-19 20:43:38 +0100

    Initial commit for commands

---
+++ Nix/Resources/Discord/Commands/MiscCommands.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Addons.Interactive;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Nix.Resources
+{
+    public class MiscCommands : NixContext
+    {
+        [Command("ping")]
+        public async Task Ping()
+            => await ReplyAsync("Pong!");
+    }
+}
