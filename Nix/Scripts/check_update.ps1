1260c8c713176211f6189d9c54dfbc314d681f08
ZitaRR <Haeggqvist1998@hotmail.com>
2021-02-01 08:58:03 +0100

    Added fetct statement

--- Nix/Scripts/check_update.ps1
+++ Nix/Scripts/check_update.ps1
@@ -2,6 +2,7 @@
 $UPSTREAM = if ($args.Length -le 0) {"origin/master"} else {"origin/" + $args[0]}
 
 try{
+    git fetch origin
     $local = git rev-parse HEAD
     $remote = git rev-parse $UPSTREAM
 }
