60a3f4a5276eac0f8f9a830809d36e2b6e893710
ZitaRR <haeggqvist1998@hotmail.com>
2026-01-29 21:40:05 +0100

    feat: initial discord - info cmd

---
+++ Nix/CommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Nix;
+
+internal class CommandHandler(DiscordSocketClient client, CommandService commands, IServiceProvider services, ILogger<CommandHandler> logger)
+{
+    public async Task InitializeAsync()
+    {
+        client.MessageReceived += HandleMessageAsync;
+        await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+    }
+
+    private async Task HandleMessageAsync(SocketMessage socketMessage)
+    {
+        if (socketMessage is not SocketUserMessage message || socketMessage.Author.IsBot)
+        {
+            return;
+        }
+
+        var argPos = 0;
+        if (!message.HasCharPrefix('!', ref argPos) &&
+            !message.HasMentionPrefix(client.CurrentUser, ref argPos))
+        {
+            return;
+        }
+
+        var context = new SocketCommandContext(client, message);
+        await commands.ExecuteAsync(context, argPos, services);
+    }
+}
--- Nix/Program.cs
+++ Nix/Program.cs
@@ -1,18 +1,33 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace Nix;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
-        if (string.IsNullOrEmpty(token))
-        {
-            Console.WriteLine("DISCORD_TOKEN environment variable not found.");
-            return;
-        }
+        var config = new ConfigurationBuilder()
+            .CreateNixConfig();
 
-        Console.WriteLine("DISCORD_TOKEN: " + token);
+        var builder = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<DiscordSocketClient>(_ => new(new DiscordSocketConfig
+                {
+                    GatewayIntents = Discord.GatewayIntents.All
+                }));
+                services.AddSingleton<CommandService>();
+                services.AddSingleton<CommandHandler>();
+
+                services.AddSingleton(typeof(NixConfig), config);
+                services.AddHostedService<NixClient>();
+            });
+
+        await builder.RunConsoleAsync();
     }
 }
